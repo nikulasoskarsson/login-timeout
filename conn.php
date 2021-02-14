4c31d5d96365417e812071efad53ffedcdc87833
Osk Bjorns <osk@Osks-MacBook-Pro.local>
2021-02-14 16:26:13 +0100

    Connection done

--- conn.php
+++ conn.php
@@ -1,1 +1,17 @@
-the connection
+<?php
+try{
+  $sDatabaseUserName = 'root';
+  $sDatabasePassword = '';
+  $sDatabaseConnection = "mysql:host=localhost; dbname=web-sec; charset=utf8mb4";
+
+  $aDatabaseOptions = array(
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_NUM
+  );
+  $db = new PDO( $sDatabaseConnection, $sDatabaseUserName, $sDatabasePassword, $aDatabaseOptions );
+}catch( PDOException $e){
+  http_response_code(500);
+  exit();
+}
