f5acc581c91a0ef3f7ab4664d2577ca88f64081b
Osk Bjorns <osk@Osks-MacBook-Pro.local>
2021-02-14 17:31:17 +0100

    login + counter

--- index.php
+++ index.php
@@ -1,61 +1,61 @@
 <?php
-if($_POST){
+
+if ($_POST) {
     session_start();
-    if(!isset($_SESSION['loginBlocked'])){
-        if(!isset($_POST['username'])){
+    if (!isset($_SESSION['loginBlocked'])) {
+        if (!isset($_POST['username'])) {
             $usernameError = 'Username needs to be set';
-        }
-        else if(strlen($_POST['username']) < 3){
+        } else if (strlen($_POST['username']) < 3) {
             $usernameError = 'Username needs to be at least 3 characters';
-        }
-        else if(strlen($_POST['username']) > 20){
+        } else if (strlen($_POST['username']) > 20) {
             $usernameError = 'Username cannot be longer then 20 characters';
-        }
-        else{
+        } else {
             $usernameError = null;
         }
-        if(!isset($_POST['password'])){
+        if (!isset($_POST['password'])) {
             $passwordError = 'password needs to be set';
-        }
-        else if(strlen($_POST['password']) < 3){
+        } else if (strlen($_POST['password']) < 3) {
             $passwordError = 'password needs to be at least 3 characters';
-        }
-        else if(strlen($_POST['password']) > 20){
+        } else if (strlen($_POST['password']) > 20) {
             $passwordError = 'password cannot be longer then 20 characters';
-        }
-        else{
+        } else {
             $passwordError = null;
         }
-        if($usernameError || $passwordError){
-            // get the current counter
-            if(isset($_SESSION['counter'])){
-                $counter = $_SESSION['counter'];
+    }
+    // No errors 
+    else {
+        require('./conn.php');
+        // var_dump($db);
+        try {
+            $q = $db->prepare('SELECT * FROM users ');
+            $q->bindValue(':username', $_POST['username']);
+            $q->execute();
+            $row = $q->fetch();
+            // var_dump($row->username);
+
+            if ($_POST['username'] == $row->username && $_POST['password'] == $row->password) {
+                echo 'Login..';
+            } else {
+                if (isset($_SESSION['counter'])) {
+                    $counter = $_SESSION['counter'];
+                } else {
+                    $counter = 3;
+                }
+                // de-increment the counter by one
+                if ($counter !== 0) {
+                    $counter--;
+                    $_SESSION['counter'] = $counter;
+                    echo $counter;
+                } else {
+                    $_SESSION['loginBlocked'] = true;
+                }
             }
-            else{
-                $counter = 3;
-            }
-            // de-increment the counter by one
-            if($counter !== 0){
-                $counter--;
-                $_SESSION['counter'] = $counter;
-                echo $counter;
-            }
-            else{
-                $_SESSION['loginBlocked'] = true;
-            }
-    
-        }
-        // No errors 
-        else{
-            require('./conn.php');
-            var_dump($db);
+
+            http_response_code(401);
+        } catch (Exception $ex) {
+            sendError(500, 'system under maintainance', __LINE__);
         }
     }
-    // login blocked
-    else{
-        
-    }
-    
 }
 
 
@@ -64,29 +64,38 @@ if($_POST){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
+
 <body>
 
-<div class="container">
-    <form action="" class="login" method="POST" <?php if(isset($_SESSION['loginBlocked'])){echo 'onsubmit="return false;"';} ?>>
-        <div class="input-group">
-            <label for="username" class="label">Username</label>
-            <input name="username" type="text" class="input">
-            <?php if(isset($usernameError)) {echo "<p class='error'>$usernameError</p>"; }?>
-        </div>
-        <div class="input-group">
-            <label for="password" class="label">password</label>
-            <input name="password" type="password" class="input">
-            <?php if(isset($passwordError)) {echo "<p class='error'>$passwordError</p>"; }?>
-        </div>
-        <button class="button">Login</button>
-    </form>
-</div>
-    
+    <div class="container">
+        <form action="" class="login" method="POST" <?php if (isset($_SESSION['loginBlocked'])) {
+                                                        echo 'onsubmit="return false;"';
+                                                    } ?>>
+            <div class="input-group">
+                <label for="username" class="label">Username</label>
+                <input name="username" type="text" class="input">
+                <?php if (isset($usernameError)) {
+                    echo "<p class='error'>$usernameError</p>";
+                } ?>
+            </div>
+            <div class="input-group">
+                <label for="password" class="label">password</label>
+                <input name="password" type="password" class="input">
+                <?php if (isset($passwordError)) {
+                    echo "<p class='error'>$passwordError</p>";
+                } ?>
+            </div>
+            <button class="button">Login</button>
+        </form>
+    </div>
+
 </body>
+
 </html>
